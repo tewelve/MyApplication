02aba4e4f70c154466d9f458242cf074b24dbd0b
tewelve <kashyaprishi4u@yahoo.com>
2017-09-10 13:59:24 +0530

    Initial commit

---
+++ app/src/main/java/com/project/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.project.performance;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.project.performance.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
